ac6e926a7313b2b64eb7fa6944282bec06dfca5f
swim-mer <kelly.talk.api@gmail.com>
2024-09-27 00:23:38 -0700

    Update dynamic-memory.h

--- dynamic-memory.h
+++ dynamic-memory.h
@@ -7,10 +7,8 @@
 #include <stdbool.h>
 
 
-extern const uint32_t HEAP_SIZE; // Used a calculator to count number of bytes between 0x80010000 - 0x80020000 inclusive
-extern uint64_t HEAP_START_ADDRESS; // Not part of the assignment
-extern const uint32_t HEAP_END_ADDRESS; // Not part of the assignment 
-extern const uint16_t BITMAP_SIZE; // Used a function to calculate the available bytes per proportion of the heap
+extern const uint32_t HEAP_SIZE;
+extern const uint16_t BITMAP_SIZE;
 extern const uint16_t USABLE_BYTES_SIZE;
 extern uint8_t heap[65537];
 
